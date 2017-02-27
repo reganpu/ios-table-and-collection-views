516fc1e09357d7e3f67a81052ce6b5b7a5bc7e53
reganpu <reganpu@gmail.com>
2017-02-27 21:12:18 +0800

    Display item 0 to item 10 in a Table View

---
+++ ch1-simple-table/ch1-simple-table/SimpleTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SimpleTableViewController.swift
+//  ch1-simple-table
+//
+//  Created by regan on 27/02/2017.
+//  Copyright © 2017 Heng-Hao. All rights reserved.
+//
+
+import UIKit
+
+// Conform the ViewController class to both the UITableViewDataSource & UITableViewDelegate 
+// protocols
+class SimpleTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // Add in a declaration for the property
+    // Create an empty array of a String type using initializer syntax
+    var tableData = [String]()
+    
+    // MARK: Load view into memory (not display)
+    // This function is executed as part of UIViewController's cycle
+    // before the view is visible on-screen
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setting up the data here will give the table the data it needs to draw itself
+        // before it becomes visible
+        for count in 0...10 {
+            // The cell will contain the string "Item X"
+            tableData.append("Item \(count)")
+        }
+        
+        print("The tableData array contains \(tableData)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UITableViewDataSource functions
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // the function is guaranteed to return a cell instance that you can work with, 
+        // so you’re ready to configure its contents.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+        // MARK: Configuring the Cell
+        // a UITableViewCell contains a UILabel outlet called textLabel
+        // that you can set the text property of
+        cell.textLabel!.text = tableData[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
